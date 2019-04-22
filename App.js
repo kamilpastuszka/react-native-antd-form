2431f60a40b17d4688302a21873f333fa8a73075
Kamil <kamil_p7@o2.pl>
2019-04-22 21:09:35 +0100

    created form

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Button from "@ant-design/react-native/lib/button";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import InputItem from "@ant-design/react-native/lib/input-item";
+import TextareaItem from "@ant-design/react-native/lib/textarea-item";
+import List from "@ant-design/react-native/lib/list";
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Button>antd button</Button>
+        <View style={styles.inputContainer}>
+          <List style={styles.listStyle}>
+            <InputItem placeholder={"First Name"} />
+            <InputItem placeholder={"Second Name"} />
+            <InputItem placeholder={"Email"} />
+            <TextareaItem
+              style={styles.textArea}
+              rows={6}
+              placeholder={"Message"}
+            />
+          </List>
+        </View>
+        <Button style={styles.button}>Submit</Button>
       </View>
     );
   }
@@ -33,16 +33,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "grey",
+    borderColor: 2,
+    borderRadius: 10
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+  inputContainer: {
+    borderRadius: 20,
+    width: "80%"
   },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  listStyle: {
+    borderRadius: 10,
+    backgroundColor: "pink"
+  },
+  button: {
+    fontSize: 50,
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "grey",
+    color: "white"
+  },
+  textArea: {
+    margin: 10,
+    borderColor: "green"
   }
 });
